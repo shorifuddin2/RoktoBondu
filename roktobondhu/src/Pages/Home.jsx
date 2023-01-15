a44e217b7bdbf5f9d79de3f7c752c67427d787bf
shorifuddin2 <shorifuddin240915@gmail.com>
2023-01-15 21:38:46 +0600

    Navbar or footer setup

--- roktobondhu/src/Pages/Home.jsx
+++ roktobondhu/src/Pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 const Home = () => {
-  return <div></div>;
+  return <div>
+      <Navbar></Navbar>
+      <h1>Home</h1>
+      <Footer></Footer>
+  </div>;
 };
 
 export default Home;
