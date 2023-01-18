ef2b7dbd8249d15afd4f50f416d50d1534f6316c
Mohammad Al Amin <alaminbamna08@gmail.com>
2023-01-18 22:44:51 +0600

    platelet update

--- roktobondhu/src/Pages/Home.jsx
+++ roktobondhu/src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "./Footer/Footer";
 import Navbar from "./Navbar/Navbar";
+import Platelet from "./platelet/Platelet";
 
 const Home = () => {
   return <div>
       <Navbar></Navbar>
       <h1>Home</h1>
+      <Platelet></Platelet>
       <Footer></Footer>
   </div>;
 };
