4c29a6c235014f917c08951f59209282143bebae
muntasir <muntasir.u@coppamet.com>
2023-01-19 15:46:24 +0600

    blogs responsive

--- roktobondhu/src/components/Common/Container.jsx
+++ roktobondhu/src/components/Common/Container.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Container = ({ children }) => {
-  return <div className="w-[1140px] m-auto">{children}</div>;
+  return (
+    <div className=" xs:w-[320px] sm:w-[380px] md:w-[940px] lg:w-[1140px] m-auto">
+      {children}
+    </div>
+  );
 };
 
 export default Container;
