d85af6043471a50b63a67fbee8b2a0241ee52526
Sourav Saha <shanto.sourav07@gmail.com>
2023-02-03 20:39:24 +0600

    hello test

--- beckend/server.js
+++ beckend/server.js
@@ -7,8 +7,9 @@ app.use(express.json());
 const port = process.env.PORT || 4000;
 
 // routes for user
-
+// hello
 // connect to MongoDB
+mongoose.set('strictQuery', true)
 mongoose
   .connect(
     "mongodb+srv://roktobondhuapi:PLKv7ExYU89w2oEp@cluster0.d89bc7r.mongodb.net/?retryWrites=true&w=majority"
